bb0b0e04d558721433b8e5ab24ffdca5018de1d9
JCarosella <juancarosella96@creativa77.com.ar>
2021-11-28 21:36:38 -0300

    Adding Python EDF app

---
+++ PyEDF-APP/EDFWorker.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+
+from numpy.core.records import array
+import pyedflib
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class EDFWorker():
+    # Class variables
+    # Flag to state if an EDF file is loaded in the system or not
+    file_loaded = False
+    # List with all the channels selected for the simulator
+    selected_channels = []
+
+    def _init_(self):
+        print("EDF Worker initialized")
+
+    def getSampleRate(self):
+        """
+        Getter for the signal sample rate
+        """
+        return self.signal_headers[0]["sample_rate"]
+
+    def getSignalDimension(self):
+        """
+        Getter for the signal unit dimension
+        """
+        return self.signal_headers[0]["dimension"]
+
+    def getNumberOfChannels(self):
+        """
+        Getter for the number of channels in the file
+        """
+        return self.physical_signals.shape[0]
+
+    def getSignalInfo(self):
+        """
+        Returns a dictionary with the signal information:
+        Currently returns: Number of channels, dimension, sample_rate.
+        """
+        signal_info_dict = {}
+        signal_info_dict["sample_rate"] = self.getSampleRate()
+        signal_info_dict["dimension"] = self.getSignalDimension()
+        signal_info_dict["number_of_channels"] = self.getNumberOfChannels()
+        return signal_info_dict
+
+    def setSelectedChannels(self, selected_channels):
+        """
+        Method to set the selected channels array
+
+        param[in]: "selected_channels" Array of integers with the selected channels
+        """
+        number_of_channels = self.getNumberOfChannels()
+        # Check the size of the input array
+        if len(selected_channels) < 0 or len(selected_channels) > number_of_channels:
+            print ("Invalid amount of selected channels")
+            return False
+        # Check that all channels are valid
+        for channel_number in selected_channels:
+            if (channel_number < 0) or (channel_number > number_of_channels - 1):
+                print ("Selected channel out of bounds")
+                return False
+        else:
+            self.selected_channels = selected_channels
+            return True
+
+    def generateDigitalSignals(self):
+        """
+        Method to generate the digital signals from the physical ones
+        """
+        self.digital_signals = np.empty([1, self.physical_signals[1]])
+        for i in range(self.physical_signals.shape[0]):
+            digital_signal = self.physicalToDigital(self.physical_signals[i],
+                                                    self.signal_headers[i]["digital_min"],
+                                                    self.signal_headers[i]["digital_max"],
+                                                    self.signal_headers[i]["physical_min"],
+                                                    self.signal_headers[i]["physical_max"])
+            self.digital_signals = np.vstack(
+                (self.digital_signals, digital_signal))
+
+    def readEDF(self, file_full_path):
+        """
+        Method to read an EDF file
+
+        param[in]: "file_full_path" The full path to the EDF file
+        returns: True if it was successful, false otherwise
+        """
+        try:
+            self.physical_signals, self.signal_headers, self.headers = pyedflib.highlevel.read_edf(
+                file_full_path, verbose=True)
+            # Clear selected channels upon reading a new file
+            self.selected_channels.clear()
+            # As a default when reading a file, select all channels
+            for signal_index in range(len(self.signal_headers)):
+                self.selected_channels.append(signal_index)
+            self.file_loaded = True
+            return True
+        except:
+            return False
+
+    def previewSignals(self):
+        """
+        Method to plot a preview of the selected signal channels for the user to view
+        """
+        if (self.file_loaded):
+            _, axis = plt.subplots(len(self.selected_channels))
+            for index in range(len(self.selected_channels)):
+                axis[index].plot(self.physical_signals[self.selected_channels[index]], color=(
+                    [168/255, 193/255, 5/255]), linewidth=0.2)
+                # Hide axis values
+                axis[index].get_xaxis().set_ticks([])
+                axis[index].get_yaxis().set_ticks([])
+                axis[index].set_title(
+                    "Channel " + str(self.selected_channels[index]))
+            # Make it fullscreen
+            plot_figure_manager = plt.get_current_fig_manager()
+            plot_figure_manager.resize(*plot_figure_manager.window.maxsize())
+            plt.show()
+        else:
+            return False
+
+    def physicalToDigital(self, signal, dmin, dmax, pmin, pmax):
+        m = (pmax-pmin) / (dmax/dmin)
+        b = pmax / m - dmax
+        digital = signal/m - b
+        return digital
---
+++ PyEDF-APP/PyEDF.py
@@ -0,0 +1,241 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import time
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from EDFWorker import EDFWorker
+
+
+class EDFSimulator(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.info_h_boxes = []
+        # Titles font
+        self.title_font = QFont()
+        self.title_font.setBold(True)
+        self.title_font.setUnderline(True)
+        self.title_font.setPointSize(15)
+        # Info/config subtitles font
+        self.info_key_font = QFont()
+        self.info_key_font.setBold(True)
+        self.info_key_font.setPointSize(14)
+        # EDF worker instance
+        self.edf_worker = EDFWorker()
+        self.initUI()
+
+    def initUI(self):
+        # ==================================== LEFT COLUMN ====================================
+        self.browse_button = QPushButton("BROWSE")
+        self.browse_button.clicked.connect(self.browseEDFFiles)
+
+        # ==================================== VERTICAL SEPARATOR LINE ====================================
+        self.separator_line = QFrame()
+        self.separator_line.setFrameShape(QFrame.VLine)
+
+        # ==================================== RIGHT COLUMN ====================================
+        self.current_file_label = QLabel("Current EDF file: ")
+        self.current_file_name_label = QLabel("")
+        self.current_file_h_layout = QHBoxLayout()
+        self.current_file_h_layout.addWidget(self.current_file_label)
+        self.current_file_h_layout.addWidget(
+            self.current_file_name_label, stretch=1)
+
+        self.separator_line_2a = QFrame()
+        self.separator_line_2a.setFrameShape(QFrame.HLine)
+
+        # Information vertical layout
+        self.information_title_label = QLabel("Signal information:")
+        self.information_title_label.setFont(self.title_font)
+        self.information_labels_layout = QVBoxLayout()
+        self.info_v_layout = QVBoxLayout()
+        self.info_v_layout.addWidget(
+            self.information_title_label, alignment=Qt.AlignHCenter)
+        self.info_v_layout.addLayout(
+            self.information_labels_layout)
+        self.info_v_layout.addStretch()
+
+        # Config vertical layout
+        # Title
+        self.config_v_layout = QVBoxLayout()
+        self.configuration_title_label = QLabel("Configuration:")
+        self.configuration_title_label.setFont(self.title_font)
+        self.config_v_layout.addWidget(
+            self.configuration_title_label, alignment=Qt.AlignHCenter)
+        # Selected channels
+        self.selected_channels_key = QLabel("Selected channels: ")
+        self.selected_channels_key.setFont(self.info_key_font)
+        self.selected_channels_value = QLabel("")
+        selected_channels_h_box = QHBoxLayout()
+        selected_channels_h_box.addWidget(self.selected_channels_key)
+        selected_channels_h_box.addWidget(
+            self.selected_channels_value, stretch=1)
+        self.config_v_layout.addLayout(selected_channels_h_box)
+        channel_selector_h_box = QHBoxLayout()
+        self.select_channels_button = QPushButton("Select")
+        self.select_channels_button.clicked.connect(self.selectChannels)
+        self.channel_select_line_edit = QLineEdit()
+        channel_selector_h_box.addWidget(self.channel_select_line_edit)
+        channel_selector_h_box.addWidget(self.select_channels_button)
+        self.config_v_layout.addLayout(channel_selector_h_box)
+        # Placeholders
+        self.placeholder_label_2 = QLabel("asd2")
+        self.placeholder_label_3 = QLabel("asd3")
+        self.config_v_layout.addWidget(
+            self.placeholder_label_2, alignment=Qt.AlignTop)
+        self.config_v_layout.addWidget(
+            self.placeholder_label_3, alignment=Qt.AlignTop)
+        self.config_v_layout.addStretch()
+
+        self.separator_line_2b = QFrame()
+        self.separator_line_2b.setFrameShape(QFrame.HLine)
+
+        self.preview_button = QPushButton("PREVIEW")
+        self.preview_button.setSizePolicy(
+            QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.preview_button.clicked.connect(self.previewEDF)
+        self.run_button = QPushButton("RUN")
+        self.run_button.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        self.run_button.clicked.connect(self.runEDFSimulator)
+        self.run_preview_buttons_h_layout = QHBoxLayout()
+        self.run_preview_buttons_h_layout.addStretch(1)
+        self.run_preview_buttons_h_layout.addWidget(self.preview_button)
+        self.run_preview_buttons_h_layout.addWidget(self.run_button)
+        self.run_preview_buttons_h_layout.addStretch(1)
+
+        # ==================================== GRID ====================================
+        main_grid = QGridLayout()
+        main_grid.setRowStretch(1, 1)
+        main_grid.setRowStretch(2, 1)
+        main_grid.setRowStretch(3, 1)
+        main_grid.setRowStretch(4, 100)
+        main_grid.setRowStretch(5, 1)
+        main_grid.setRowStretch(6, 10)
+        main_grid.addWidget(self.browse_button, 1, 1, alignment=Qt.AlignTop)
+        main_grid.addWidget(self.separator_line, 1, 2, 6, 1)
+        main_grid.addLayout(self.current_file_h_layout,
+                            1, 3, alignment=Qt.AlignTop)
+        main_grid.addWidget(self.separator_line_2a, 2, 3)
+        main_grid.addLayout(self.info_v_layout, 3, 3)
+        main_grid.addLayout(self.config_v_layout, 4, 3)
+        main_grid.addWidget(self.separator_line_2b, 5, 3)
+        main_grid.addLayout(self.run_preview_buttons_h_layout, 6, 3)
+
+        self.setLayout(main_grid)
+
+        self.setGeometry(300, 300, 1000, 800)
+        self.setWindowTitle('EDF Simulator')
+
+        self.centerMainWindow()
+        self.show()
+
+    # ==================================== CLASS METHODS ====================================
+    def centerMainWindow(self):
+        """
+        Class method to center the main window in the user screen
+        """
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def browseEDFFiles(self):
+        """
+        Callback method for the "browse" button press.
+
+        This method will try to load the EDF file clicked by the user
+        into the system and display the corresponding information.
+        """
+        # Filter only for EDF files
+        filter = "EDFFiles(*.edf)"
+        file_name = QFileDialog.getOpenFileName(
+            self, "Select EDF file", os.getcwd(), filter)[0]
+        # Load EDF file into worker
+        if(self.edf_worker.readEDF(file_name)):
+            # Place the file name in the dialog box
+            self.current_file_name_label.setText(file_name)
+            # Delete info h layouts in the info v layout (not the title)
+            for widget_index in range(self.information_labels_layout.count()):
+                self.deleteBoxFromLayout(
+                    self.information_labels_layout, self.info_h_boxes[widget_index])
+            # Generate the information h boxes and add them to the info v layout
+            signal_info_dict = self.edf_worker.getSignalInfo()
+            self.info_h_boxes.clear()
+            for key in signal_info_dict:
+                info_key = QLabel(str(key) + ": ")
+                info_key.setFont(self.info_key_font)
+                info_value = QLabel(str(signal_info_dict[key]))
+                h_box = QHBoxLayout()
+                h_box.addWidget(info_key)
+                h_box.addWidget(info_value, stretch=1)
+                self.info_h_boxes.append(h_box)
+                self.information_labels_layout.addLayout(h_box)
+        else:
+            print("Error when trying to load the EDF file")
+
+    def previewEDF(self):
+        """
+        Callback method for the "preview" button press.
+
+        This method will plot the selected signal channels and show it to the user
+        """
+        print("Preview EDF requested")
+        if(self.edf_worker.previewSignals() == False):
+            print("Error in preview EDF signal. Check if a file was loaded")
+
+    def runEDFSimulator(self):
+        print("Run EDF simulator requested")
+
+    def selectChannels(self):
+        """
+        Callback method for the select channels button
+
+        This method reads the selected channels line edit, parses it into an array of
+        integers and set the EDF worker with it
+        """
+        raw_selected_channels_string = self.channel_select_line_edit.text()
+        selected_channels = self.parseSelectedChannelsString(
+            raw_selected_channels_string)
+        if(self.edf_worker.setSelectedChannels(selected_channels) == False):
+            print("Error when trying to set the selected channels in the EDF worker")
+
+    def deleteBoxFromLayout(self, layout, box):
+        """
+        Method to delete a box layout from another layout
+        """
+        for i in range(layout.count()):
+            layout_item = layout.itemAt(i)
+            if layout_item.layout() == box:
+                self.deleteItemsOfLayout(layout_item.layout())
+                layout.removeItem(layout_item)
+                break
+
+    def deleteItemsOfLayout(self, layout):
+        """
+        Method to delete the items of a layout
+        """
+        if layout is not None:
+            while layout.count():
+                item = layout.takeAt(0)
+                widget = item.widget()
+                if widget is not None:
+                    widget.setParent(None)
+                else:
+                    self.deleteItemsOfLayout(item.layout())
+
+    def parseSelectedChannelsString(self, raw_selected_channels_string):
+        """
+        Method to parse the user input selected channel string into an array of ints
+        """
+
+def main():
+    app = QApplication(sys.argv)
+    ex = EDFSimulator()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
