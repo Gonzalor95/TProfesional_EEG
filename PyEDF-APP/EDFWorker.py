780b2aeb6c71c6e99e41c29b9bc40bb302bc5b4b
Hitalio <31748589+Hitalio@users.noreply.github.com>
2021-12-14 15:00:47 -0300

    Update EDFWorker.py

--- PyEDF-APP/EDFWorker.py
+++ PyEDF-APP/EDFWorker.py
@@ -9,12 +9,28 @@ import matplotlib.pyplot as plt
 class EDFWorker():
     # Class variables
     # Flag to state if an EDF file is loaded in the system or not
-    file_loaded = False
+    file_loaded_ = False
     # List with all the channels selected for the simulator
-    selected_channels = []
+    selected_channels_ = []
+    # Selected simulation time
+    selected_sim_time_ = ()
+    # Flag to indicate if digital signals were generated or not
+    digital_signals_generated_ = False
 
     def _init_(self):
         print("EDF Worker initialized")
+
+    def isFileLoaded(self):
+        """
+        Method to know if a file was loaded into the worker or not
+        """
+        return self.file_loaded_
+
+    def getSelectedSimTime(self):
+        """
+        Getter for the selected simulation time
+        """
+        return self.selected_sim_time_
 
     def getSampleRate(self):
         """
@@ -34,55 +50,55 @@ class EDFWorker():
         """
         return self.physical_signals.shape[0]
 
+    def getDuration(self):
+        """
+        Getter for the measurement duration in seconds
+        """
+        return self.physical_signals.shape[1] / self.getSampleRate()
+
     def getSignalInfo(self):
         """
         Returns a dictionary with the signal information:
         Currently returns: Number of channels, dimension, sample_rate.
         """
         signal_info_dict = {}
-        signal_info_dict["sample_rate"] = self.getSampleRate()
-        signal_info_dict["dimension"] = self.getSignalDimension()
-        signal_info_dict["number_of_channels"] = self.getNumberOfChannels()
+        signal_info_dict["Sample rate"] = self.getSampleRate()
+        signal_info_dict["Dimension"] = self.getSignalDimension()
+        signal_info_dict["Number of channels"] = self.getNumberOfChannels()
+        signal_info_dict["Duration [s]"] = self.getDuration()
         return signal_info_dict
 
     def setSelectedChannels(self, selected_channels):
         """
         Method to set the selected channels array
-
         param[in]: "selected_channels" Array of integers with the selected channels
         """
-        number_of_channels = self.getNumberOfChannels()
-        # Check the size of the input array
-        if len(selected_channels) < 0 or len(selected_channels) > number_of_channels:
-            print ("Invalid amount of selected channels")
-            return False
-        # Check that all channels are valid
-        for channel_number in selected_channels:
-            if (channel_number < 0) or (channel_number > number_of_channels - 1):
-                print ("Selected channel out of bounds")
+        if self.file_loaded_:
+            number_of_channels = self.getNumberOfChannels()
+            # Check the size of the input array
+            if len(selected_channels) < 0 or len(selected_channels) > number_of_channels:
+                print("Invalid amount of selected channels")
                 return False
-        else:
-            self.selected_channels = selected_channels
+            # Check that all channels are valid
+            for channel_number in selected_channels:
+                if (channel_number < 0) or (channel_number > number_of_channels - 1):
+                    print("Selected channel out of bounds")
+                    return False
+            self.selected_channels_ = selected_channels
             return True
+        else:
+            print("File not loaded. Cannot select number of channels")
+            return False
 
-    def generateDigitalSignals(self):
+    def setSelectedSimTime(self, selected_sim_time):
         """
-        Method to generate the digital signals from the physical ones
+        Setter for the selected simulation time
         """
-        self.digital_signals = np.empty([1, self.physical_signals[1]])
-        for i in range(self.physical_signals.shape[0]):
-            digital_signal = self.physicalToDigital(self.physical_signals[i],
-                                                    self.signal_headers[i]["digital_min"],
-                                                    self.signal_headers[i]["digital_max"],
-                                                    self.signal_headers[i]["physical_min"],
-                                                    self.signal_headers[i]["physical_max"])
-            self.digital_signals = np.vstack(
-                (self.digital_signals, digital_signal))
+        self.selected_sim_time_ = selected_sim_time
 
     def readEDF(self, file_full_path):
         """
         Method to read an EDF file
-
         param[in]: "file_full_path" The full path to the EDF file
         returns: True if it was successful, false otherwise
         """
@@ -90,39 +106,68 @@ class EDFWorker():
             self.physical_signals, self.signal_headers, self.headers = pyedflib.highlevel.read_edf(
                 file_full_path, verbose=True)
             # Clear selected channels upon reading a new file
-            self.selected_channels.clear()
+            self.selected_channels_.clear()
             # As a default when reading a file, select all channels
             for signal_index in range(len(self.signal_headers)):
-                self.selected_channels.append(signal_index)
-            self.file_loaded = True
+                self.selected_channels_.append(signal_index)
+            self.file_loaded_ = True
             return True
         except:
             return False
 
-    def previewSignals(self):
+    def previewSignals(self, signal_type):
         """
-        Method to plot a preview of the selected signal channels for the user to view
+        Method to plot a preview of the specified signal
         """
-        if (self.file_loaded):
-            _, axis = plt.subplots(len(self.selected_channels))
-            for index in range(len(self.selected_channels)):
-                axis[index].plot(self.physical_signals[self.selected_channels[index]], color=(
-                    [168/255, 193/255, 5/255]), linewidth=0.2)
-                # Hide axis values
-                axis[index].get_xaxis().set_ticks([])
-                axis[index].get_yaxis().set_ticks([])
-                axis[index].set_title(
-                    "Channel " + str(self.selected_channels[index]))
-            # Make it fullscreen # TODO: check that maxsize() does not work on windows
-            #plot_figure_manager = plt.get_current_fig_manager()
-            #plot_figure_manager.resize(*plot_figure_manager.window.maxsize())
-            plt.show()
+        if (self.file_loaded_):
+            if signal_type == "digital":
+                self.generateDigitalSignals()
+                self.plotSignals(self.digital_signals)
+                return True
+            elif signal_type == "physical":
+                self.plotSignals(self.physical_signals)
+                return True
+            else:
+                print("Incorrect type of signal specified to the preview method")
+                return False
         else:
             print("EDF file not loaded, cannot preview signals")
             return False
 
-    def physicalToDigital(self, signal, dmin, dmax, pmin, pmax):
-        m = (pmax-pmin) / (dmax/dmin)
-        b = pmax / m - dmax
-        digital = signal/m - b
-        return digital
+    def generateDigitalSignals(self): #TODO: When stacking, channels get disorder
+        """
+        Method to generate the digital signals from the physical ones
+        """
+        if not self.digital_signals_generated_:
+            self.digital_signals = np.empty([1, len(self.physical_signals[1])])
+            for i in range(self.physical_signals.shape[0]):
+                digital_signal = pyedflib.highlevel.phys2dig(self.physical_signals[i],
+                                                             self.signal_headers[i]["digital_min"],
+                                                             self.signal_headers[i]["digital_max"],
+                                                             self.signal_headers[i]["physical_min"],
+                                                             self.signal_headers[i]["physical_max"])
+                self.digital_signals = np.vstack(
+                    (self.digital_signals, digital_signal))
+            self.digital_signals_generated_ = True
+
+    def plotSignals(self, signal):
+        _, axis = plt.subplots(len(self.selected_channels_), squeeze=False)
+        for index in range(len(self.selected_channels_)):
+            signal_to_print = signal[self.selected_channels_[
+                index]][self.selected_sim_time_[0]:self.selected_sim_time_[1]]
+            axis[index][0].plot(signal_to_print, color=(
+                [168/255, 193/255, 5/255]), linewidth=0.4)
+            # Hide axis values
+            axis[index][0].get_xaxis().set_ticks([])
+            axis[index][0].get_yaxis().set_ticks([])
+            # Set plot title
+            axis[index][0].set_ylabel(
+                "Channel " + str(self.selected_channels_[index]), rotation=0, labelpad=30)
+            # Adjust axis range to better fit the signal
+            axis[index][0].set_xlim(
+                [0, self.selected_sim_time_[1] - self.selected_sim_time_[0]])
+        # Make it fullscreen # TODO: check that maxsize() does not work on windows
+        plot_figure_manager = plt.get_current_fig_manager()
+        plot_figure_manager.resize(
+            *plot_figure_manager.window.maxsize())
+        plt.show()
