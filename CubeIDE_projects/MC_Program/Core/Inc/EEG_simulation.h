1899d640ded984c004dfd92185db63b74f36acfc
Gonzalor95 <gonzalorizzo95@gmail.com>
2022-06-14 23:44:51 -0300

    Refactor: sine wave test function and libraries

--- CubeIDE_projects/MC_Program/Core/Inc/EEG_simulation.h
+++ CubeIDE_projects/MC_Program/Core/Inc/EEG_simulation.h
@@ -11,20 +11,38 @@
 
 #include "stm32f4xx_hal.h"
 
-typedef u32int_t Channel_DataPackage;
+#define DAC_CHANNEL_A_ADDR_16Bit_MASK 0x0000 // 0b0000000000000000
+#define DAC_CHANNEL_B_ADDR_16Bit_MASK 0x1000 // 0b0001000000000000
+#define DAC_CHANNEL_C_ADDR_16Bit_MASK 0x2000 // 0b0010000000000000
+#define DAC_CHANNEL_D_ADDR_16Bit_MASK 0x3000 // 0b0011000000000000
+#define DAC_CHANNEL_E_ADDR_16Bit_MASK 0x4000 // 0b0100000000000000
+#define DAC_CHANNEL_F_ADDR_16Bit_MASK 0x5000 // 0b0101000000000000
+#define DAC_CHANNEL_G_ADDR_16Bit_MASK 0x6000 // 0b0110000000000000
+#define DAC_CHANNEL_H_ADDR_16Bit_MASK 0x7000 // 0b0111000000000000
+
+typedef uint16_t dac_channel_addr_16bit_mask;
+
+typedef uint8_t Channel_DataPackage;
 
 typedef enum {
-	CHANNEL_FP1,
-	CHANNEL_FZ,
-	CHANNEL_FP2,
-	CHANNEL_F3,
-	CHANNEL_F4,
-	CHANNEL_C3,
-	CHANNEL_C4,
-	CHANNEL_P3
-}CHANNEL_TAG;
+	CHANNEL_A = 0, //0b000
+	CHANNEL_B = 1, //0b001
+	CHANNEL_C = 2, //0b010
+	CHANNEL_D = 3, //0b011
+	CHANNEL_E = 4, //0b100
+	CHANNEL_F = 5, //0b101
+	CHANNEL_G = 6, //0b110
+	CHANNEL_H = 7 //0b111
+}DAC_CHANNEL_ADDR;
 
-typedef u32int_t DAC_Config;
+typedef enum {
+	DAC_A,
+	DAC_B,
+	DAC_C,
+	DAC_D
+}DAC_TAG;
+
+typedef uint16_t DAC_Config;
 
 void config_DACs();
 void waiting_simulation_config();
@@ -33,6 +51,8 @@ void release_latch();
 void feed_DAC();
 void idle_mode();
 void execute_config();
+
+void test_sine_wave_1DAC_1Channel(DAC_CHANNEL_ADDR DAC_channel_tag,SPI_HandleTypeDef *hspi);
 
 
 #endif /* INC_EEG_SIMULATION_H_ */
