0a8bf53c7a8aaf13204cfd8642ea0471446e2e0f
Gonzalor95 <gonzalorizzo95@gmail.com>
2023-02-15 19:20:38 -0300

    comment Juan test waves

--- PyEDF-APP/modules/signalGenerator.py
+++ PyEDF-APP/modules/signalGenerator.py
@@ -10,7 +10,41 @@ Class to create required signals like:
 - sine wave
 - triangular wave
 """
+"""
+Codigo de señales tuki
 
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plot
+
+def get_sin_wave(amplitude, frecuency, time_span, sample_rate):
+    time = np.arange(0, time_span, time_span / sample_rate)
+    return time, amplitude * np.sin(2*np.pi * frecuency * time)
+
+def get_square_wave(amplitude, frecuency, time_span, sample_rate):
+    time = np.arange(0, time_span, time_span / sample_rate)
+    return time, amplitude *  signal.square(2*np.pi * frecuency * time)
+
+def get_triangular_wave(amplitude, frecuency, time_span, sample_rate):
+    time = np.arange(0, time_span, time_span / sample_rate)
+    return time, amplitude *  signal.sawtooth(2*np.pi * frecuency * time, 0.5)
+
+amplitude = 1
+frecuency = 100
+time_span = 1
+sample_rate = 10000
+# time, wave = get_sin_wave(amplitude, frecuency, time_span, sample_rate)
+# time, wave = get_square_wave(amplitude, frecuency, time_span, sample_rate)
+time, wave = get_triangular_wave(amplitude, frecuency, time_span, sample_rate)
+
+# When frecuency >= sample rate I'm getting a weird behavior I cannot explain
+# When sending the signal to the microcontroller we should do it following the same frecuency used to create the signals
+
+plot.plot(time, wave)
+plot.show()
+
+
+"""
 
     
 class SignalToGenerate():
@@ -33,25 +67,31 @@ class SignalToGenerate():
     """
     Generator for timestamp
     """
-    def timestamp (start, stop, step):
+    def timestamp (self, start, stop):
         t = [start]
         while t[-1] < stop:
-            t.append(t[-1] + step)
+            t.append(t[-1] + self.sample_rate)
         return t
 
     def _apply_function(self,t):
         return 0
 
-    def generateSignal(self, start, stop, sample_rate):
-        t = self.timestamp(start, stop, sample_rate)
+    def generateSignal(self, start, stop):
+        t = self.timestamp(start=start, stop=stop)
         y = self._apply_function()
 
 class PulseTrain(SignalToGenerate):
 
-  def __init__(self, amplitude = 0,freq = 0,sample_rate = 0):
-    super().__init__(amplitude,freq,sample_rate,signal_type_name= "Pulse Train")
+    def __init__(self, amplitude = 0,freq = 0,sample_rate = 0):
+        super().__init__(amplitude,freq,sample_rate,signal_type_name= "Pulse Train")
 
-
+    def _apply_function(self, t):
+        return t +5
 
 if __name__ == '__main__':
     os.system('cls') ## Clear screen
+
+    pulseTrain = PulseTrain(amplitude=1,freq=3,sample_rate=5)
+    y = pulseTrain.generateSignal(start=0,stop=4)
+    plt.plot(y)
+    plt.show()
